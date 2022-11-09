68e5776448e079532b1c6536915422bf9626643f
elvina22222 <114909756+elvina22222@users.noreply.github.com>
2022-11-10 00:02:55 +0400

    hover

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-const arrowdown = document.querySelector('.arrowDown');
-arrowdown.addEventListener('mouseover', (event) => {
-    event.target.style.color = 'black';
-});
-arrowdown.addEventListener('mouseout', (event) => {
-    event.target.style.color = '#C4C4C4';
-});
+// const arrowdown = document.querySelector('.arrowDown');
+// arrowdown.addEventListener('mouseover', (event) => {
+//     event.target.style.color = 'black';
+// });
+// arrowdown.addEventListener('mouseout', (event) => {
+//     event.target.style.color = '#C4C4C4';
+// });
 
-const krest= document.querySelector('.krestik');
+// const whiteCross = document.querySelector('.whitecross')
+// whiteCross.addEventListener('mouseover', (event) => {
+//     whiteCross.addEventListener('change' , purpleCross)
+//  });  
+    // function purpleCross () {
+    //   var whitecross = getElementByClassName("whitecross");
+    // const whitecross = document.querySelector('.whitecross');
+    //   if (whitecross.src == "image/arrowDown.svg" ) {
+    //     whitecross.src == "image/whitecross.svg"
+    //   } else {
+    //     whitecross.src == "image/arrowDown.svg" ;
+    //   }
+    //}
 
-krest.addEventListener('mouseover', (event) => {
-    event.target.style.color = '#833AE0';
-});
-krest.addEventListener('mouseout', (event) => {
-    event.target.style.color = 'white';
-});
+
+
+// document.querySelector("#purplerectangle").click = function(){
+    // tbl = document.createElement('table');
+    // tbl.style.width = '940px';
+    // tbl.style.height = '135px';
+    // tbl.style.color = 'red';
+    // tbl.style.border = '5px solid #C4C4C4';
+    
+//   }
+
+  const btn = document.querySelector('button');
+  btn.onclick = function() {
+    tbl = document.createElement('table');
+    smallrectangle.add (tbl)
+  }
+  
